6b14d34e98a8e5f59dd0ed0460fd536652f37ec0
adrianaMurillo19 <adrianal.murillot@uqvirtual.edu.co>
2026-05-24 16:11:49 -0500

    Trigger_cuentaActiva - Corregido

--- trigger_cuentaActiva.sql
+++ trigger_cuentaActiva.sql
@@ -5,6 +5,8 @@ ADD (
     estado_cuenta VARCHAR2(20)
 );
 
+UPDATE usuario set estado_cuenta = 'INACTIVO' WHERE id_usuario in(SELECT id_usuario from suscripcion where fecha_final < sysdate);
+update usuario set estado_cuenta = 'ACTIVO' where id_usuario in (select id_usuario from suscripcion where fecha_final > sysdate);
 
 CREATE OR REPLACE TRIGGER trg_cuenta_activa
 AFTER INSERT ON Pago
